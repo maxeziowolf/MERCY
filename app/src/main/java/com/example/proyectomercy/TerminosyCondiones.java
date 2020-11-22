4cacaf38683d3a753f9d37b65b95fff3085d683c
maxeziowolf <63032627+maxeziowolf@users.noreply.github.com>
2020-11-21 23:27:16 -0600

    Version 1
    Una version incompleta del programa

---
+++ app/src/main/java/com/example/proyectomercy/TerminosyCondiones.java
@@ -0,0 +1,14 @@
+package com.example.proyectomercy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TerminosyCondiones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminosy_condiones);
+    }
+}
