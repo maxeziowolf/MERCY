6f372ae9a10fb5b2c67acc4ff1d0efc40ed03f4e
maxeziowolf <63032627+maxeziowolf@users.noreply.github.com>
2020-11-22 22:14:45 -0600

    VERSION 1.1
    Es una version incomleta, solo pantallas y en esta version se programo el boton regresar de T&C

--- app/src/main/java/com/example/proyectomercy/TerminosyCondiones.java
+++ app/src/main/java/com/example/proyectomercy/TerminosyCondiones.java
@@ -2,7 +2,9 @@ package com.example.proyectomercy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TerminosyCondiones extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TerminosyCondiones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminosy_condiones);
+    }
+
+    public void regresar(View view){
+        Intent i =new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
